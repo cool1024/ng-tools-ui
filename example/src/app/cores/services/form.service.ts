0fd86a895247399955cca6138525920bae9a7581
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-06-12 09:54:22 +0800

    update some docs

--- example/src/app/cores/services/form.service.ts
+++ example/src/app/cores/services/form.service.ts
@@ -1,3 +1,9 @@
+/**
+ * 表单服务，提供了一些表单提交的辅助方法
+ * @file form.service.ts
+ * @author xiaojian
+ * @date 2018年06月12日
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -7,6 +13,8 @@ export class FormService {
 
     /**
      * 深度拷贝:不能出现循环引用
+     * @param {{ [key: string]: any }} obj 需要拷贝的对象
+     * @return {{ [key: string]: any }
      */
     copy(obj: { [key: string]: any }): { [key: string]: any } {
         return JSON.parse(JSON.stringify(obj));
@@ -14,6 +22,8 @@ export class FormService {
 
     /**
      * 浅拷贝一个JSON数据
+     * @param {{ [key: string]: any }} obj 需要拷贝的对象
+     * @return {{ [key: string]: any }
      */
     jsonCopy(json: { [key: string]: any }): { [key: string]: any } {
         const copy: { [key: string]: any } = {};
--- example/src/app/cores/services/global.service.ts
+++ example/src/app/cores/services/global.service.ts
@@ -1,3 +1,9 @@
+/**
+ * 全局服务，提供了系统内部全局变量，存储方法
+ * @file global.service.ts
+ * @author xiaojian
+ * @date 2018年06月12日
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -5,14 +11,31 @@ export class GlobalService {
 
     private values: { [key: string]: any } = {};
 
+    /**
+     * 获取一个系统全局变量
+     * @param {string} key 变量名称
+     * @param {any} defaultValue 默认值
+     * @return {any}
+     */
     getValue(key: string, defaultValue: any = ''): any {
         return this.values.hasOwnProperty(key) ? this.values[key] : defaultValue;
     }
 
+    /**
+     * 设置一个系统全局变量
+     * @param {string} key 变量名称
+     * @param {any} value 值
+     * @return {void}
+     */
     setValue(key: string, value: any) {
         this.values[key] = value;
     }
 
+    /**
+     * 批量设置系统全局变量
+     * @param {{ [key: string]: any }} params 批量参数
+     * @return {void}
+     */
     setValues(params: { [key: string]: any }) {
         for (const key in params) {
             if (params.hasOwnProperty(key)) {
@@ -21,10 +44,21 @@ export class GlobalService {
         }
     }
 
+    /**
+     * 从本地存储中获取一个变量
+     * @param {string} key 变量名称
+     * @param {string} defaultValue 默认值
+     * @return {string}
+     */
     getValueFromStorage(key: string, defaultValue = ''): string {
         return localStorage.getItem(key) || defaultValue;
     }
 
+    /**
+     * 从本地存储中批量获取变量
+     * @param {string[]} ...keys 变量名称
+     * @return {{ [key: string]: string }}
+     */
     getValuesFromStorage(...keys: string[]): { [key: string]: string } {
         const params: any = {};
         keys.forEach(key => {
@@ -33,6 +67,11 @@ export class GlobalService {
         return params;
     }
 
+    /**
+     * 批量校验本地存储中的变量，一个不存在就会校验失败
+     * @param {string[]} ...keys 变量名称
+     * @return {boolean} 
+     */
     checkValuesFromStorage(...keys: string[]): boolean {
         return keys.findIndex(key => {
             const value = this.getValueFromStorage(key);
@@ -40,10 +79,21 @@ export class GlobalService {
         }) < 0;
     }
 
+    /**
+     * 设置本地存储
+     * @param {string} key 键名
+     * @param {string} value 值
+     * @return {void}
+     */
     setValueToStorage(key: string, value: string) {
         localStorage.setItem(key, value);
     }
 
+    /**
+     * 批量设置本地存储
+     * @param {{ [key: string]: any }} keyValues 批量参数
+     * @return {void}
+     */
     setValuesToStorage(keyValues: { [key: string]: string }) {
         for (const key in keyValues) {
             if (keyValues.hasOwnProperty(key)) {
@@ -52,15 +102,28 @@ export class GlobalService {
         }
     }
 
+    /**
+     * 从本地存储中加载一个字符串（只能是string）
+     * @param {string} key 键名
+     * @param {string} defaultValue 默认值
+     */
     loadStrFromStorage(key: string, defaultValue: string) {
         this.setValue(key, this.getValueFromStorage(key, defaultValue));
     }
 
+    /**
+     * 从本地存储中加载一个数字（只能是Int）
+     * @param {string} key 键名
+     * @param {number} defaultValue 默认值
+     */
     loadIntFromStorage(key: string, defaultValue: number) {
         const temp = this.getValueFromStorage(key, defaultValue.toString());
         this.setValue(key, parseInt(temp, 10));
     }
 
+    /**
+     * 清空本地存储
+     */
     cleanAllStorage() {
         localStorage.clear();
     }
